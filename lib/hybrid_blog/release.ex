72cc95227558b4381973bcf8132ad4d65e0d4860
IKEDA Hiroki <ikeda_hiroki@icloud.com>
2020-05-28 01:13:06 +0900

    Revert "start :ssl before migration"
    This reverts commit 72ecd4f72464a7286d3cbe038c30e941a8bbe3eb.

--- lib/hybrid_blog/release.ex
+++ lib/hybrid_blog/release.ex
@@ -1,7 +1,7 @@
 defmodule HybridBlog.Release do
   @app :hybrid_blog
   def migrate(_argv) do
-    start_apps()
+    Application.load(@app)
 
     for repo <- Application.fetch_env!(@app, :ecto_repos) do
       {:ok, _, _} =
@@ -15,15 +15,10 @@ defmodule HybridBlog.Release do
   end
 
   def rollback(argv) do
-    start_apps()
+    Application.load(@app)
     {options, _, _} = OptionParser.parse(argv, strict: [repo: :string, version: :integer])
     repo = Keyword.fetch!(options, :repo)
     version = Keyword.fetch!(options, :version)
     {:ok, _, _} = Ecto.Migrator.with_repo(repo, &Ecto.Migrator.run(&1, :down, to: version))
-  end
-
-  defp start_apps do
-    Application.load(@app)
-    Application.ensure_started(:ssl)
   end
 end
