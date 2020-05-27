7741f855efea08fe92e1c2474262864c40ee9ca4
IKEDA Hiroki <ikeda_hiroki@icloud.com>
2020-05-28 01:23:13 +0900

    messages around custom commands

--- rel/commands/migrate.sh
+++ rel/commands/migrate.sh
@@ -1,2 +1,4 @@
 #!/bin/sh
+info "starting migration..."
 release_ctl eval --mfa "HybridBlog.Release.migrate/1" --argv -- "$@"
+success "migration completed."
--- rel/commands/rollback.sh
+++ rel/commands/rollback.sh
@@ -1,2 +1,4 @@
 #!/bin/sh
+info "starting rollback..."
 release_ctl eval --mfa "HybridBlog.Release.rollback/1" --argv -- "$@"
+success "rollback completed."
