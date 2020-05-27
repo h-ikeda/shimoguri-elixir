71e000dbbbda91cce088c73b1ac3038618ee5ca0
IKEDA Hiroki <ikeda_hiroki@icloud.com>
2020-05-27 15:51:54 +0900

    title helper

--- test/hybrid_blog_web/controllers/page_controller_test.exs
+++ test/hybrid_blog_web/controllers/page_controller_test.exs
@@ -1,8 +1,16 @@
 defmodule HybridBlogWeb.PageControllerTest do
   use HybridBlogWeb.ConnCase
 
-  test "GET /", %{conn: conn} do
-    conn = get(conn, "/")
-    assert html_response(conn, 200) =~ "Welcome to Phoenix!"
+  describe "GET /" do
+    test "\"Welcome to Phoenix\" in the body string", %{conn: conn} do
+      conn = get(conn, "/")
+      assert html_response(conn, 200) =~ "Welcome to Phoenix!"
+    end
+
+    test "title in the html", %{conn: conn} do
+      conn = get(conn, "/")
+      expected_title = Application.get_env(:hybrid_blog, :title)
+      assert html_response(conn, 200) =~ "<title>#{expected_title}<\/title>"
+    end
   end
 end
--- test/hybrid_blog_web/views/layout_view_test.exs
+++ test/hybrid_blog_web/views/layout_view_test.exs
@@ -5,4 +5,14 @@ defmodule HybridBlogWeb.LayoutViewTest do
   # use functions such as safe_to_string() to convert the helper
   # result into an HTML string.
   # import Phoenix.HTML
+  alias HybridBlogWeb.LayoutView
+
+  test "title/1 without subtitle" do
+    assert LayoutView.title(nil) == Application.get_env(:hybrid_blog, :title)
+  end
+
+  test "title/1 with subtitle" do
+    assert LayoutView.title("Sub title 123") ==
+             "Sub title 123 | #{Application.get_env(:hybrid_blog, :title)}"
+  end
 end
