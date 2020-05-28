2061d0db4a423608194131f066d0e87df66a12b3
IKEDA Hiroki <ikeda_hiroki@icloud.com>
2020-05-28 12:09:26 +0900

    temporaly disable forcing ssl

--- config/prod.exs
+++ config/prod.exs
@@ -46,7 +46,7 @@ config :logger, level: :info
 # We also recommend setting `force_ssl` in your endpoint, ensuring
 # no data is ever sent via http, always redirecting to https:
 #
-config :hybrid_blog, HybridBlogWeb.Endpoint, force_ssl: [rewrite_on: [:x_forwarded_proto]]
+# config :hybrid_blog, HybridBlogWeb.Endpoint, force_ssl: [rewrite_on: [:x_forwarded_proto]]
 
 #
 # Check `Plug.SSL` for all available options in `force_ssl`.
