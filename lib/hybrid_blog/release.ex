4f80ff2cdad262143173120b3758e612725387b9
IKEDA Hiroki <ikeda_hiroki@icloud.com>
2020-05-28 17:41:51 +0900

    fix typo

--- lib/hybrid_blog/release.ex
+++ lib/hybrid_blog/release.ex
@@ -11,7 +11,7 @@ defmodule HybridBlog.Release do
   def rollback(argv) do
     {:ok, _} = Application.ensure_all_started(:ssl)
     {options, _, _} = OptionParser.parse(argv, strict: [repo: :string, version: :integer])
-    repo = Module.concat(Macro.camelize(@sapp), Keyword.fetch!(options, :repo))
+    repo = Module.concat(Macro.camelize(@app), Keyword.fetch!(options, :repo))
     version = Keyword.fetch!(options, :version)
     {:ok, _, _} = Ecto.Migrator.with_repo(repo, &Ecto.Migrator.run(&1, :down, to: version))
   end
