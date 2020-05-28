0d3ec8fa80c47e15782ac601e81ad9e1739118be
IKEDA Hiroki <ikeda_hiroki@icloud.com>
2020-05-28 11:36:24 +0900

    any hosts to be forced ssl on same hosts

--- config/prod.exs
+++ config/prod.exs
@@ -46,7 +46,8 @@ config :logger, level: :info
 # We also recommend setting `force_ssl` in your endpoint, ensuring
 # no data is ever sent via http, always redirecting to https:
 #
-config :hybrid_blog, HybridBlogWeb.Endpoint, force_ssl: [rewrite_on: [:x_forwarded_proto]]
+config :hybrid_blog, HybridBlogWeb.Endpoint,
+  force_ssl: [rewrite_on: [:x_forwarded_proto], host: nil]
 
 #
 # Check `Plug.SSL` for all available options in `force_ssl`.
