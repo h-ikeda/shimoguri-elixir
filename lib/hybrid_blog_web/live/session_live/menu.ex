6888a13d777b56dff5cb581e1cd375ada9933616
IKEDA Hiroki <ikeda_hiroki@icloud.com>
2020-07-07 15:45:39 +0900

    alias is not efficient

--- lib/hybrid_blog_web/live/session_live/menu.ex
+++ lib/hybrid_blog_web/live/session_live/menu.ex
@@ -1,9 +1,8 @@
 defmodule HybridBlogWeb.SessionLive.Menu do
   use HybridBlogWeb, :live_view
-  alias HybridBlog.Accounts
   @impl true
   def mount(:not_mounted_at_router, %{"current_user_id" => current_user_id}, socket) do
-    %{name: name, picture: picture} = Accounts.get_user!(current_user_id)
+    %{name: name, picture: picture} = HybridBlog.Accounts.get_user!(current_user_id)
     if connected?(socket), do: HybridBlogWeb.Endpoint.subscribe("user:#{current_user_id}")
     {:ok, assign(socket, user_id: current_user_id, name: name, picture: picture)}
   end
