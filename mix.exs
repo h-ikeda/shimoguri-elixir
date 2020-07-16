df63f5aa16d4b7fc63c81ab0dac9f84f28c92a79
IKEDA Hiroki <ikeda_hiroki@icloud.com>
2020-07-17 00:31:16 +0900

    Update patch version

--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule HybridBlog.MixProject do
   def project do
     [
       app: :hybrid_blog,
-      version: "0.1.5",
+      version: "0.1.6",
       elixir: "~> 1.7",
       elixirc_paths: elixirc_paths(Mix.env()),
       compilers: [:phoenix, :gettext] ++ Mix.compilers(),
