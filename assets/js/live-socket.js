4e30b177db0c10774470526538e4a44a0f6fced7
IKEDA Hiroki <ikeda_hiroki@icloud.com>
2020-08-21 16:22:33 +0900

    Convenient dev env (#81)
    * chore: conveniently building a development environment
    
    * fix: correct commands
    
    * fix: correct copy path
    
    * chore: run several install commands after builds
    
    * chore: add a command to setup postgres
    
    * chore: add a dependency package
    
    * chore: displays a kind error message

--- assets/js/live-socket.js
+++ assets/js/live-socket.js
@@ -7,11 +7,18 @@ const liveSocket = new LiveSocket("/live", Socket, {
   hooks: {
     authorize: {
       async mounted() {
-        const response = await fetch('/api/authorize_url')
-        const data = await response.json()
-        Object.entries(data).forEach(([provider, url]) => {
-          this.el.querySelector(`a.${provider}`).href = url
-        })
+        try {
+          const response = await fetch('/api/authorize_url')
+          const data = await response.json()
+          Object.entries(data).forEach(([provider, url]) => {
+            this.el.querySelector(`a.${provider}`).href = url
+          })
+        } catch {
+          const p = document.createElement('p')
+          p.textContent = 'Couldn\'t fetch authorize URLs.'
+          p.className = 'px-6 text-xs text-red-600'
+          this.el.insertBefore(p, this.el.querySelector('a:first-of-type'))
+        }
       }
     }
   }
